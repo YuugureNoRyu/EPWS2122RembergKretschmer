4384e3b926a6578615ef087bbdbd8ce2b67452c9
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2021-11-25 16:02:58 +0100

    Interactable /grab interaction

---
+++ first Unity Tests/A Deep Dive (Test)/Assets/Gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+public float speed =40;
+public GameObject bullet;
+public Transform Waffe;
+
+  public void Schießen()
+  {
+      GameObject spawnedBullet = Instantiate(bullet,Waffe.position,Waffe.rotation);
+      spawnedBullet.GetComponent<Rigidbody>().velocity=speed*Waffe.forward;
+      Destroy(spawnedBullet,2);
+  }
+}
---
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/Gun.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+public float speed =40;
+public GameObject bullet;
+public Transform Waffe;
+
+  public void Fire()
+  {
+      GameObject spawnedBullet=Instantiate(bullet,Waffe.position,Waffe.rotation);
+      spawnedBullet.GetComponent<Rigidbody>().velocity=speed*Waffe.forward;
+      Destroy(spawnedBullet,2);
+  }
+}
