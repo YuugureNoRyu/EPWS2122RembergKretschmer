3562beb0d638d641ea369841b47cab020f676e2c
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2021-12-14 17:19:47 +0100

    Erste Animationen und interaktionen mit den Fischen

---
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/Animation_Pause.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animation_Pause : MonoBehaviour
+{public Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim=GetComponent<Animation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnHoverEnter(Collider other)
+        {
+            anim.enabled=false;
+            foreach(AnimationState state in anim )
+           { state.speed = 0F;
+            }
+        }
+
+        private void OnHoverExit(Collider other)
+        {
+            anim.enabled=true;
+             foreach(AnimationState state in anim )
+           { state.speed = 1F;
+            }
+     
+        }
+}
