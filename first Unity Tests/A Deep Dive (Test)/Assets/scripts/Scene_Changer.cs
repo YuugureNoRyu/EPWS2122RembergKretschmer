faa062a5380ec63bab204b794a0bf650ad7d2491
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2021-12-17 16:04:57 +0100

    Main Menu Scene

---
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/Scene_Changer.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;  
+public class Scene_Changer: MonoBehaviour {  
+    public void Menu() {  
+        SceneManager.LoadScene("Main-Scene");  
+    }  
+    
+}   
