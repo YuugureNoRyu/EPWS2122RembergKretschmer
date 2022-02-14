9af1e04479eff1bf0eadce1ff0472bac14ad35dc
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2022-02-14 15:50:46 +0100

    abtauchen angefangen

---
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/Player/ForceMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMove : MonoBehaviour
+{
+ public GameObject Player;
+ public void Update()
+    {
+        Player.transform.position=transform.TransformPoint(0,0,1);
+    }
+}
