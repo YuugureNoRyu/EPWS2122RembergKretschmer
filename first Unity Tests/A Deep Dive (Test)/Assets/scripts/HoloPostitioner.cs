a28be4e3c3946cc7047a8d484792e7df49126787
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2022-02-14 15:19:42 +0100

    Wristband "fertig" interaktion möglich

---
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/HoloPostitioner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoloPostitioner : MonoBehaviour
+{
+  public GameObject Holomodell;
+public Vector3 pos;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    
+            Holomodell.transform.position=transform.TransformPoint(pos);
+        
+    }
+}
