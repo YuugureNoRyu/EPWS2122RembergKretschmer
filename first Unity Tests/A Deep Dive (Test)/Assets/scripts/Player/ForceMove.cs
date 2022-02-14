1ec34043a838211cf808c6d9140ba683cda8b80c
Yannik Kretschmer <yannik_leon.kretschmer@smail.th-koeln.de>
2022-02-14 16:39:45 +0100

    Menu abtauchen

--- first Unity Tests/A Deep Dive (Test)/Assets/scripts/Player/ForceMove.cs
+++ first Unity Tests/A Deep Dive (Test)/Assets/scripts/Player/ForceMove.cs
@@ -1,12 +1,44 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ForceMove : MonoBehaviour
-{
- public GameObject Player;
- public void Update()
-    {
-        Player.transform.position=transform.TransformPoint(0,0,1);
-    }
+{    public float targetTime = 10f;
+     
+    public XRNode inputSource;
+private XRRig rig;
+private CharacterController character;
+public float speed =1;
+public bool sceneStarten=false;
+
+
+    
+
+ 
+ public void Update(){
+
+  targetTime -= Time.deltaTime;
+ 
+ if (targetTime >= 0.0f)
+ {
+     character= GetComponent<CharacterController>();
+        rig=GetComponent<XRRig>();
+
+    
+    
+         Quaternion headYaw=Quaternion.Euler(0,rig.cameraGameObject.transform.eulerAngles.y,0);
+         Vector3 direction = headYaw * new Vector3(0, -1f,0);
+         
+    character.Move(direction * Time.fixedDeltaTime * speed);
+    
+ }
+ 
+ 
+ 
+ 
 }
+}
+
+
